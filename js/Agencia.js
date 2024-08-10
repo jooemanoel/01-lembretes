91c3ddd6283829e66357038fb1a9ea830b9bd52a
jooemanoel <jooemanoel@gmail.com>
2024-08-09 23:11:59 -0300

    Agora sim tudo zerado

--- js/Agencia.js
+++ js/Agencia.js
@@ -1,16 +1,7 @@
 import { Componente } from "./Componente.js";
 export class Agencia {
     listaContainers = [];
-    conteudosHTML = [
-        {
-            titulo: `João - Treinamento`,
-            texto: `<h1 class="principal__conteudo__texto">Responsividade em Páginas Web</h1><p class="principal__conteudo__texto">Ao desenvolver páginas da internet, uma boa prática é ajustar os elementos para que se ajustem a diferentes tamanhos de tela, seja com o uso de unidades relativas ou até com a alteração do layout da página para exibição em telas mais estreitas.</p><p class="principal__conteudo__texto">Todas as configurações de ajuste da página podem ser feitas em CSS, e alteradas dinamicamente em determinadas situações com o auxílio de Javascript. Contudo, o próprio CSS nos permite criar estilizações de página excelentes.</p>`
-        },
-        {
-            titulo: `Sobre Mim`,
-            texto: `<h1 class="principal__conteudo__texto">Breve Histórico:</h1><p class="principal__conteudo__texto">Nascido em 1993, tenho atualmente 31 anos. Após 10 anos atuando como controlador de tráfego aéreo, estou iniciando uma nova carreira no BB, sendo designado inicialmente para atuar como desenvolvedor front-end.</p><p class="principal__conteudo__texto">Durante as primeiras semanas de treinamento, tive a oportunidade de realizar ótimos cursos na plataforma Alura para desenvolvimento com HTML e CSS, o que me permitiu criar esta página.</p>`
-        }
-    ];
+    conteudosHTML = [];
     constructor() {
         const leitura = JSON.parse(localStorage.getItem('lembretes'));
         console.log(leitura);
